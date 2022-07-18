ab768eeee3be3e2a82a5e17576b7cdf39ed01f70
NHUJI <cnzhouchang@gmail.com>
2022-07-18 17:34:22 +0800

    done with project0,but have some bugs

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -5,7 +5,7 @@ import java.util.Observable;
 
 
 /** The state of a game of 2048.
- *  @author TODO: YOUR NAME HERE
+ *  @author
  */
 public class Model extends Observable {
     /** Current contents of the board. */
@@ -16,6 +16,8 @@ public class Model extends Observable {
     private int maxScore;
     /** True iff game is ended. */
     private boolean gameOver;
+    //测试使用
+    static Board a;
 
     /* Coordinate System: column C, row R of the board (where row 0,
      * column 0 is the lower-left corner of the board) will correspond
@@ -113,11 +115,53 @@ public class Model extends Observable {
         // TODO: Modify this.board (and perhaps this.score) to account
         // for the tilt to the Side SIDE. If the board changed, set the
         // changed local variable to true.
+        board.setViewingPerspective(side);
+        for (int c = 0; c < board.size(); c += 1) {
+//        处理一行的函数
+            //从一个col的最上面的第二个tile开始循环
+            int Size = board.size();
+            boolean isMerge[][] ;
+            isMerge = new boolean[Size][Size];
+            for (int r = board.size()-2;r>=0;r=r-1) {
+                Tile t = board.tile(c, r);
 
+                int up = 0;
+
+                if (t != null) {
+
+                    for (int row = r+1;row < board.size();row++){
+                        if(board.tile(c,row) == null){
+                            //如果上面是空的向上移动一步
+                            up = up+1;
+                        } else {
+                            //如果上面有值并且没有被合并过就判断是否相等
+                            if(!isMerge[c][row]){
+                        if(t.value()== board.tile(c,row).value()){
+                            //上面一格不是空的就比较大小，相同就向上移动一步
+                           up = up+1;
+                            score += t.value()*2;
+                        }}
+                        }}}
+//               最终决定向上移动几步
+                if(up != 0){
+                    //在原有的基础上向上移动几格，并记录该格已经被合并过了
+                isMerge[c][up+r]=board.move(c,r+up,t);
+                changed = true;
+
+                }
+            }
+
+        }
+
+
+        //每次计算结束后记得更新
+
+        //结束我的代码
         checkGameOver();
         if (changed) {
             setChanged();
         }
+        board.setViewingPerspective(Side.NORTH);
         return changed;
     }
 
@@ -138,7 +182,32 @@ public class Model extends Observable {
      * */
     public static boolean emptySpaceExists(Board b) {
         // TODO: Fill in this function.
-        return false;
+        boolean existEmpty = false;
+//        System.out.println(b.size());
+//        System.out.println(b.tile(1,0));
+       // 遍历整个board看看是否有0的方块
+        for (int i = 0;i < b.size();i++)
+        {
+            for (int j = 0;j < b.size();j++){
+               if(b.tile(i,j) == null) {
+                    existEmpty = true ;
+               }
+            }
+
+        }
+
+        //测试
+
+//        int[][] rawVals = new int[][] {
+//                {13, 14, 15, 16},
+//                {9, 10, 11, 12},
+//                {5, 6, 7, 8},
+//                {1, 2, 3, 4},
+//        };
+//
+//        a = new Board(rawVals, 0);
+//        System.out.println(a.tile(1,0));
+        return existEmpty;
     }
 
     /**
@@ -148,7 +217,23 @@ public class Model extends Observable {
      */
     public static boolean maxTileExists(Board b) {
         // TODO: Fill in this function.
-        return false;
+        boolean maxTileExists = false ;
+        for (int i = 0;i < b.size();i++)
+        {
+            for (int j = 0;j < b.size();j++){
+                if (b.tile(i,j) == null){
+
+                }
+                else if (b.tile(i,j).value() == MAX_PIECE) {
+                    maxTileExists = true ;
+                }
+
+            }
+
+        }
+
+//        System.out.println(b.tile(0,3).value());
+        return maxTileExists;
     }
 
     /**
@@ -158,8 +243,34 @@ public class Model extends Observable {
      * 2. There are two adjacent tiles with the same value.
      */
     public static boolean atLeastOneMoveExists(Board b) {
-        // TODO: Fill in this function.
-        return false;
+        boolean validMove = false;
+        //针对有空位和值可以合并两种来判断
+        if(emptySpaceExists(b)){
+            //既然有空的tile存在就可以直接返回可以移动了，不用再做接下来的循环，也避免了将一个有值的方块与无值的对比，结果不能移动
+            return true ;
+        }
+        // 将每个tile和上下左右的值进行对比，如果有相同的就修改
+        for (int i = 0;i < b.size();i++)
+        {
+            for (int j = 0;j < b.size();j++){
+                //先判断tile有没有上下左右的方块 再通过if来验证里面的值
+
+                if(i<b.size()-1){
+                    if(b.tile(i,j).value() == b.tile(i+1,j).value()){validMove = true;}
+                }
+                if(i>0){
+                    if(b.tile(i,j).value() == b.tile(i-1,j).value()){validMove = true;}
+                }
+                if(j<b.size()-1){
+                    if(b.tile(i,j).value() == b.tile(i,j+1).value()){validMove = true;}
+                }
+                if(j>0){
+                    if(b.tile(i,j).value() == b.tile(i,j-1).value()){validMove = true;}
+                }
+            }
+
+        }
+        return validMove;
     }
 
 
